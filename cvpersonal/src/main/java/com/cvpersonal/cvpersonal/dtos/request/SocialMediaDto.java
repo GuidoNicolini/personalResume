33b863b8465bb706a7fbdf220ffa1ea4c709f964
GuidoNicolini <guidonicolini123@hotmail.com>
2023-06-03 19:25:36 -0300

    Lombok annotations added

--- cvpersonal/src/main/java/com/cvpersonal/cvpersonal/dtos/request/SocialMediaDto.java
+++ cvpersonal/src/main/java/com/cvpersonal/cvpersonal/dtos/request/SocialMediaDto.java
@@ -1,4 +1,13 @@
 package com.cvpersonal.cvpersonal.dtos.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class SocialMediaDto {
 }
