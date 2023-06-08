e4f2b2aed413dd4daf99e3fc48a83b892aa06745
GuidoNicolini <guidonicolini123@hotmail.com>
2023-06-08 15:27:32 -0300

    The basic structure has been added

---
+++ cvpersonal/src/main/java/com/cvpersonal/cvpersonal/controllers/EducationController.java
@@ -0,0 +1,11 @@
+package com.cvpersonal.cvpersonal.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/education")
+public class EducationController {
+}
---
+++ cvpersonal/src/main/java/com/cvpersonal/cvpersonal/controllers/WorkExperienceController.java
@@ -0,0 +1,12 @@
+
+package com.cvpersonal.cvpersonal.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/workexperience")
+public class WorkExperienceController {
+}
