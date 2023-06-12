e0bc3b0c774b6dd035fb5db0a8741de0a52a78d4
GuidoNicolini <guidonicolini123@hotmail.com>
2023-06-12 19:35:25 -0300

    security basic

---
+++ cvpersonal/src/main/java/com/cvpersonal/cvpersonal/security/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.cvpersonal.cvpersonal.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("GuidoUser")
+                .password(bCryptPasswordEncoder.encode("Asdasd12"))
+                .roles("USER")
+                .build());
+        manager.createUser(User.withUsername("GuidoAdmin")
+                .password(bCryptPasswordEncoder.encode("Asdasd12"))
+                .roles("USER", "ADMIN")
+                .build());
+        return manager;
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest().hasAnyRole("USER","ADMIN")
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+
+}
